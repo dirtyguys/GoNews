9c500e8e114f6db4a2e3da272a2ef54ad257c3d4
mkfsn <p408865@gmail.com>
2015-10-16 01:12:41 +0900

    Simple server for serving home page and some static files

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/go-martini/martini"
+    "github.com/martini-contrib/render"
+)
+
+func main() {
+    m := martini.Classic()
+    // render html templates from templates directory
+    m.Use(render.Renderer())
+    m.Use(martini.Static("assets"))
+
+    m.Get("/", func(r render.Render) {
+        r.HTML(200, "home", "")
+    })
+    m.Run()
+}
+
+
+
